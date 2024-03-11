11b122dd56e07ea6ad420e9d1c4dd4f5ab2a431d
brobo1 <armanahz95@gmail.com>
2024-03-11 15:18:22 +0100

    Refactor App to pass authors and posts to Post components; add Roboto font and styling for Post.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import { Post } from "./components/Post.tsx";
 
+const names: string[] = ["Alice", "Bob", "Charlie", "Dave", "Eve"];
+
+const posts: string[] = [
+  "Just had a great workout! Feeling energized and ready for the day.",
+  "Enjoying a beautiful sunset. The colors are just breathtaking.",
+  "Cooked a delicious meal today. It was a new recipe and it turned out great.",
+  "Spending quality time with family. These moments are precious.",
+  "Working on an exciting new project. Can't wait to share more details soon.",
+];
+
 function App() {
   return (
     <main>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post author={names} body={posts} />
+      <Post author={names} body={posts} />
+      <Post author={names} body={posts} />
+      <Post author={names} body={posts} />
     </main>
   );
 }
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,19 +1,17 @@
-const names: string[] = ["Alice", "Bob", "Charlie", "Dave", "Eve"];
-const posts: string[] = [
-  "Just had a great workout! Feeling energized and ready for the day.",
-  "Enjoying a beautiful sunset. The colors are just breathtaking.",
-  "Cooked a delicious meal today. It was a new recipe and it turned out great.",
-  "Spending quality time with family. These moments are precious.",
-  "Working on an exciting new project. Can't wait to share more details soon.",
-];
+import styles from "./Post.module.css";
 
-export function Post() {
-  const chosenName: number = Math.floor(Math.random() * names.length);
+type PostProps = {
+  author: string[];
+  body: string[];
+};
+
+export function Post({ author, body }: PostProps) {
+  const chosenName: number = Math.floor(Math.random() * author.length);
 
   return (
-    <div>
-      <p>{names[chosenName]}</p>
-      <p>{posts[chosenName]}</p>
+    <div className={styles.post}>
+      <p className={styles.author}>{author[chosenName]}</p>
+      <p className={styles.text}>{body[chosenName]}</p>
     </div>
   );
 }
